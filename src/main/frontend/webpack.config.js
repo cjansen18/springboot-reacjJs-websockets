124d2d153621635d627e3b002f6ac5f265d4a4d5
Chris Jasnen <cjansen18@gmail.com>
2017-09-04 13:44:29 -0400

    Initial commit

---
+++ src/main/frontend/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    devServer :{
+        port: 9090
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, exclude: /node_modules/,loaders: ['style-loader', 'css-loader'],},
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
+
